7ce8ef35fc8191d2241db94d8bc32882e6866731
raldin14 <raldin.hidalgo@gmail.com>
2025-07-28 08:31:01 -0500

    Updating lab6

--- Lab6/JpaOptimization/src/main/java/app/CustomerRepository.java
+++ Lab6/JpaOptimization/src/main/java/app/CustomerRepository.java
@@ -1,9 +1,21 @@
 package app;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("select c from Customer c")
+    List<Customer> findAllWithoutAccounts();
 
+    @Modifying
+    @Transactional
+    @Query("update Customer c set c.name = :name")
+    int updateNames(@Param("name") String name);
 }
 
 
